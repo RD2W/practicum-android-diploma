5b02fc429d26aec7ae57915e84eb46928525594f
Maksim Krutovertsev <m.krutovertsev@yadro.com>
2025-06-06 00:32:27 +0300

    Исправить замечания после запуска detekt

--- app/src/main/java/ru/practicum/android/diploma/app/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/app/App.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import ru.practicum.android.diploma.BuildConfig
 import timber.log.Timber
 
-class App: Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
