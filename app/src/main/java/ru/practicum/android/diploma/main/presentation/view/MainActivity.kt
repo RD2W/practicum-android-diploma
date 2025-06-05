654ba90eb9dc7eb96987b6d604d72d493ace2b9f
Maksim Krutovertsev <m.krutovertsev@yadro.com>
2025-06-05 23:33:29 +0300

    Создать базовую структуру пакетов в проекте
    1. Давлена структура пакетов проекта
    2. Добавлены атрибуты для ориентации экрана и поведения клавиатуры в манифест
    3. Добавлены необходимые разрешения в манифест

--- app/src/main/java/ru/practicum/android/diploma/main/presentation/view/MainActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/main/presentation/view/MainActivity.kt
@@ -1,14 +1,14 @@
-package ru.practicum.android.diploma.ui.root
+package ru.practicum.android.diploma.main.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
-class RootActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        setContentView(R.layout.activity_main)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
