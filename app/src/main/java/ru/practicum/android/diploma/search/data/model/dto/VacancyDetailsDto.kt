902da53e24769e176b020b65472c88c644ce78a5
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2025-06-08 10:06:17 +0300

    fix necessary fields in vacancy details dto

--- app/src/main/java/ru/practicum/android/diploma/search/data/model/dto/VacancyDetailsDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/model/dto/VacancyDetailsDto.kt
@@ -3,19 +3,21 @@ package ru.practicum.android.diploma.search.data.model.dto
 import com.google.gson.annotations.SerializedName
 
 data class VacancyDetailsDto(
-    val id: String,
-    val area: AreaDto,
-    val address: AddressDto? = null,
-    val description: String,
-    val employer: EmployerDto? = null,
+    val id: String, // Идентификатор вакансии
+    val area: AreaDto,  // Регион
+    val address: AddressDto? = null,    // Адрес
+    val description: String,    // Описание в html
+    val employer: EmployerDto? = null,  // Поля работодателя в вакансии
     @SerializedName("employment_form")
     val employmentForm: EmploymentDto? = null, // Тип занятости
-    val experience: ExperienceDto? = null,
+    val experience: ExperienceDto? = null,  // Опыт работы
     @SerializedName("key_skills")
-    val keySkills: List<String>? = null,
-    val name: String,
+    val keySkills: List<String>? = null,    // Список ключевых навыков
+    val name: String,   // Название
     @SerializedName("salary_range")
-    val salaryRange: SalaryDto? = null,
+    val salaryRange: SalaryDto? = null, // Зарплата
     @SerializedName("work_format")
     val workFormat: WorkFormatDto? = null, // Формат работы
+    @SerializedName("alternate_url")
+    val alternateUrl: String? = null,   // Ссылка на представление вакансии на сайте
 )
