83fee2477c6a258521ef65e628e96f063efcd876
Michel-mihim <ya-mihim@yandex.ru>
2025-06-08 23:20:56 +0300

    salary_class_added

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/model/Filter.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/model/Filter.kt
@@ -1,7 +1,9 @@
 package ru.practicum.android.diploma.filter.domain.model
 
+import ru.practicum.android.diploma.salary.domain.model.Salary
+
 data class Filter(
     val areaId: String?,
     val industryId: String?,
-    //val salary: Salary?
+    val salary: Salary?
 )
