f8bac1e0467c748668253d61563b73569b86b73f
Michel-mihim <ya-mihim@yandex.ru>
2025-06-08 23:49:58 +0300

    salary_class_corrected

--- app/src/main/java/ru/practicum/android/diploma/salary/domain/model/Salary.kt
+++ app/src/main/java/ru/practicum/android/diploma/salary/domain/model/Salary.kt
@@ -1,8 +1,8 @@
 package ru.practicum.android.diploma.salary.domain.model
 
 data class Salary(
-    val currency: String,
-    val from: Int,
-    val gross: Boolean,
+    val currency: String = "RUR",
+    val from: Int = 0,
+    val gross: Boolean = true,
     val to: Int?
 )
