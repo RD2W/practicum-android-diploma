f89e277bff288df08f73c8b10de71a70fbde9fbd
RD2W <mkrutovercev@yandex.ru>
2025-06-08 23:24:37 +0300

    Настроить DI (Koin) для NetworkClient, репозиториев и ViewModel`s

--- app/src/main/java/ru/practicum/android/diploma/common/constants/AppConstants.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/constants/AppConstants.kt
@@ -4,4 +4,6 @@ object AppConstants {
     const val NOT_AVAILABLE = "N/A"
     const val CLICK_DEBOUNCE_DELAY_MILLIS = 1000L
     const val SEARCH_DEBOUNCE_DELAY_MILLIS = 2000L
+    const val SHARED_PREFERENCES = "app_preferences"
+    const val BASE_URL = "https://api.hh.ru/"
 }
