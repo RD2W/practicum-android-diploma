3c9a267612845d2aba38db66c3bb636a1049f490
unknown <ya-mihim@yandex.ru>
2025-06-09 14:28:19 +0300

    di_search_and_filter_usecases_added

--- app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
@@ -2,6 +2,10 @@ package ru.practicum.android.diploma.di
 
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
+import ru.practicum.android.diploma.filter.domain.usecase.GetAreasListUseCase
+import ru.practicum.android.diploma.filter.domain.usecase.GetCountriesListUseCase
+import ru.practicum.android.diploma.filter.domain.usecase.GetIndustriesListUseCase
+import ru.practicum.android.diploma.search.domain.usecase.GetVacanciesUseCase
 import ru.practicum.android.diploma.team.domain.usecase.GetDevTeamUseCase
 import ru.practicum.android.diploma.team.domain.usecase.OpenGithubProfileUseCase
 
@@ -9,4 +13,8 @@ val domainModule = module {
     /** Developers Team UseCases */
     factoryOf(::GetDevTeamUseCase)
     factoryOf(::OpenGithubProfileUseCase)
+    factoryOf(::GetVacanciesUseCase)
+    factoryOf(::GetCountriesListUseCase)
+    factoryOf(::GetAreasListUseCase)
+    factoryOf(::GetIndustriesListUseCase)
 }
