6aedc5fc10a467d2d1af8e3100f40c76fb58e2b4
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2025-06-13 12:21:21 +0300

    refactor: optimize imports (found by detekt)

--- app/src/main/java/ru/practicum/android/diploma/search/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/repository/SearchRepositoryImpl.kt
@@ -2,13 +2,13 @@ package ru.practicum.android.diploma.search.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.common.domain.model.RequestResult
 import ru.practicum.android.diploma.search.data.mapper.toMap
 import ru.practicum.android.diploma.search.data.mapper.toSearchResult
 import ru.practicum.android.diploma.search.data.mapper.toVacancyDetails
 import ru.practicum.android.diploma.search.data.model.HHApiRequest
 import ru.practicum.android.diploma.search.data.model.HHApiResponse
 import ru.practicum.android.diploma.search.data.source.remote.NetworkClient
-import ru.practicum.android.diploma.common.domain.model.RequestResult
 import ru.practicum.android.diploma.search.domain.model.SearchParameters
 import ru.practicum.android.diploma.search.domain.model.SearchResult
 import ru.practicum.android.diploma.search.domain.repository.SearchRepository
