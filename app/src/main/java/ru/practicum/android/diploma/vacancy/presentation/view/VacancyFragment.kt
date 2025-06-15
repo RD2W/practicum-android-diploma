5350d47b0da47d1b6add239c29790d85cfa80c86
Annet <annamrkova2802@gmail.com>
2025-06-15 23:58:00 +0400

    viewModel's states visibility

--- app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/view/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/view/VacancyFragment.kt
@@ -8,12 +8,16 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.common.utils.FormatStrings
 import ru.practicum.android.diploma.databinding.FragmentVacancyBinding
+import ru.practicum.android.diploma.vacancy.domain.model.VacancyDetails
 import ru.practicum.android.diploma.vacancy.presentation.state.VacancyDetailsState
+import ru.practicum.android.diploma.vacancy.presentation.state.VacancyErrorType
 import ru.practicum.android.diploma.vacancy.presentation.viewmodel.VacancyViewModel
 import kotlin.getValue
 
@@ -48,17 +52,87 @@ class VacancyFragment : Fragment(R.layout.fragment_vacancy) {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.state.collectLatest { state ->
                     when (state) {
-                        is VacancyDetailsState.Loading -> { }
+                        is VacancyDetailsState.Loading -> { showLoading() }
                         is VacancyDetailsState.Content -> {
+                            showVacancy(state.vacancy)
                             updateFavoriteIcon(state.isFavorite)
                         }
                         is VacancyDetailsState.Error -> {
+                            showError(state.errorType)
                             updateFavoriteIcon(state.isFavorite)
                         }
                     }
                 }
             }
         }
+    }
+
+    private fun showLoading() {
+        with(binding) {
+            scroll.visibility = View.GONE
+            errorContainer.visibility = View.GONE
+            progressIndicator.visibility = View.VISIBLE
+            progressIndicator.isIndeterminate = true
+        }
+    }
+
+    private fun showVacancy(vacancyDetails: VacancyDetails) {
+
+
+
+        with(binding) {
+
+            scroll.visibility = View.VISIBLE
+            errorContainer.visibility = View.GONE
+            progressIndicator.visibility = View.GONE
+
+            vacancyTitle.text = vacancyDetails.titleOfVacancy
+            vacancySalary.text = vacancyDetails.salary
+            vacancySphere.text = vacancyDetails.employerName
+            vacancyCity.text = vacancyDetails.regionName
+            vacancyExperienceMeaning.text = vacancyDetails.experience
+            vacancySchedule.text = vacancyDetails.scheduleType
+
+            vacancyDescription.text = FormatStrings.htmlToFormattedText(vacancyDetails.description.toString())
+            vacancyKeySkills.text = FormatStrings.htmlToFormattedText(vacancyDetails.keySkills.toString())
+
+            Glide.with(binding.root.context)
+                .load(vacancyDetails.employerLogoUrl)
+                .placeholder(R.drawable.ic_vacancy_placeholder)
+                .fitCenter()
+                .centerCrop()
+                .into(binding.vacancyIcon)
+        }
+
+    }
+
+    private fun showError(vacancyError: VacancyErrorType) {
+
+        with (binding) {
+
+            scroll.visibility = View.GONE
+            errorContainer.visibility = View.VISIBLE
+            progressIndicator.visibility = View.GONE
+
+            textError.text = resources.getString(
+                when (vacancyError) {
+                    VacancyErrorType.SERVER_ERROR -> R.string.server_error
+                    VacancyErrorType.NOT_FOUND -> R.string.vacancy_not_found_error
+                    VacancyErrorType.NO_INTERNET -> R.string.no_internet
+                }
+            )
+
+            errorImage.setImageResource(
+                when(vacancyError) {
+                    VacancyErrorType.SERVER_ERROR -> R.drawable.image_placeholder_server_error_vacancy
+                    VacancyErrorType.NOT_FOUND -> R.drawable.image_placeholder_deleted_vacancy
+                    VacancyErrorType.NO_INTERNET -> R.drawable.image_placeholder_no_internet
+                }
+            )
+
+        }
+
+
     }
 
     private fun setupButtonListeners() {
