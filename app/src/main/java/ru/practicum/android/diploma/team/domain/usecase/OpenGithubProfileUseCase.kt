5c1a2af16dfc85ac3c46d0f85f20b1147112decf
RD2W <mkrutovercev@yandex.ru>
2025-06-17 09:36:07 +0300

    Добавить интерфейсы для юскейсов экрана "Команада"

--- app/src/main/java/ru/practicum/android/diploma/team/domain/usecase/OpenGithubProfileUseCase.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/domain/usecase/OpenGithubProfileUseCase.kt
@@ -1,15 +1,12 @@
 package ru.practicum.android.diploma.team.domain.usecase
 
-import ru.practicum.android.diploma.team.domain.repository.DevTeamRepository
-
 /**
- * UseCase для открытия профиля GitHub
- * @property repository Репозиторий для работы с данными о команде
+ * Интерфейс для UseCase открытия профиля GitHub
  */
-class OpenGithubProfileUseCase(private val repository: DevTeamRepository) {
+interface OpenGithubProfileUseCase {
     /**
      * Вызывает открытие профиля GitHub
      * @param githubLink Ссылка на профиль GitHub
      */
-    operator fun invoke(githubLink: String) = repository.openGithubProfile(githubLink)
+    operator fun invoke(githubLink: String)
 }
