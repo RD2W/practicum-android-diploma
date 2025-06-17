068637f7b3afb4d1e0d16e246b6ebde77ddfc3ff
Michel-mihim <ya-mihim@yandex.ru>
2025-06-17 22:46:43 +0300

    Созданы классы в пакете Salary.

--- app/src/main/java/ru/practicum/android/diploma/salary/domain/model/Salary.kt
+++ app/src/main/java/ru/practicum/android/diploma/salary/domain/model/Salary.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.salary.domain.model
 
 data class Salary(
     val currency: String = "RUR",
-    val from: Int = 0,
-    val gross: Boolean = true,
-    val to: Int?
+    val from: Int? = null,
+    val gross: Boolean? = true,
+    val to: Int? = null
 )
