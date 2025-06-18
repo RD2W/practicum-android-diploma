3461de0454480bfb11dea644986b69c85edf2b8f
Michel-mihim <ya-mihim@yandex.ru>
2025-06-18 19:07:03 +0300

    Запросы фильтра работают.

--- app/src/main/java/ru/practicum/android/diploma/filter/data/mapper/AreaMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/mapper/AreaMapper.kt
@@ -1,13 +1,12 @@
 package ru.practicum.android.diploma.filter.data.mapper
 
 import ru.practicum.android.diploma.region.domain.model.Area
+import ru.practicum.android.diploma.region.domain.model.AreaRegion
 import ru.practicum.android.diploma.search.data.model.dto.AreaDto
 
-fun AreaDto.toArea(): Area {
-    return Area(
+fun AreaDto.toAreaRegion(): AreaRegion {
+    return AreaRegion(
         id = this.id,
-        parentId = this.parentId,
-        name = this.name,
-        areas = null,
+        name = this.name
     )
 }
--- app/src/main/java/ru/practicum/android/diploma/region/data/repository/GetAreasListRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/region/data/repository/GetAreasListRepositoryImpl.kt
@@ -3,15 +3,12 @@ package ru.practicum.android.diploma.region.data.repository
 import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import ru.practicum.android.diploma.filter.data.mapper.toArea
+import ru.practicum.android.diploma.filter.data.mapper.toAreaRegion
 import ru.practicum.android.diploma.region.domain.model.GetAreasListResult
 import ru.practicum.android.diploma.region.domain.repository.GetAreasListRepository
-import ru.practicum.android.diploma.region.domain.model.Area
 import ru.practicum.android.diploma.search.data.model.HHApiRequest
 import ru.practicum.android.diploma.search.data.model.HHApiResponse
-import ru.practicum.android.diploma.search.data.model.dto.AreaDto
 import ru.practicum.android.diploma.search.data.source.remote.NetworkClient
-import timber.log.Timber
 import java.net.HttpURLConnection
 
 class GetAreasListRepositoryImpl(
@@ -21,10 +18,8 @@ class GetAreasListRepositoryImpl(
         val response = networkClient.doRequest(HHApiRequest.Areas(countryId))
         when (response.responseCode) {
             HttpURLConnection.HTTP_OK -> {
-                Log.d("wtf",((response as HHApiResponse.Areas).chosenArea.areas).toString())
-
-
-                emit(GetAreasListResult.Problem)
+                val areaRegions = (response as HHApiResponse.Areas).chosenArea.areas!!.map { areaDto -> areaDto.toAreaRegion() }
+                emit(GetAreasListResult.Success(areaRegions))
             }
 
             else -> {
