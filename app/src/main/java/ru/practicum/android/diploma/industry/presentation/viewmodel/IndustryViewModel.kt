75da5c710e09af874e19487cfedfff0f21e8c692
unknown <ya-mihim@yandex.ru>
2025-06-18 14:06:27 +0300

    Скорректировано отображение результата на экране ОТРАСЛЕЙ после запроса на сервер

--- app/src/main/java/ru/practicum/android/diploma/industry/presentation/viewmodel/IndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/industry/presentation/viewmodel/IndustryViewModel.kt
@@ -18,6 +18,7 @@ class IndustryViewModel(
     fun observeIndustryFragmentState(): LiveData<IndustryFragmentState> = industryFragmentStateLiveData
 
     fun getIndustries() {
+        renderIndustryFragment(IndustryFragmentState.Loading)
         viewModelScope.launch {
             getIndustriesListUseCase.execute().collect { result ->
                 when (result) {
@@ -31,22 +32,6 @@ class IndustryViewModel(
                 }
             }
         }
-        /*
-        val i1 = Industry("1", "Инженеринг")
-        val i2 = Industry("2", "Строительство")
-        val i3 = Industry("3", "Медицина")
-        val i4 = Industry("4", "Образование")
-        val i5 = Industry("5", "Наука")
-        val i6 = Industry("6", "Полезные ископаемые")
-        val i7 = Industry("7", "Дизайн")
-        val i8 = Industry("8", "Блоггинг")
-        val i9 = Industry("9", "СМИ")
-        val i10 = Industry("10", "Силовые структуры")
-        val i11 = Industry("11", "Сельское хозяйство")
-        val i12 = Industry("12", "Туризм")
-        val list = listOf(i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12)
-        renderIndustryFragment(IndustryFragmentState.Content(list))
-        */
     }
 
     private fun renderIndustryFragment(state: IndustryFragmentState) {
