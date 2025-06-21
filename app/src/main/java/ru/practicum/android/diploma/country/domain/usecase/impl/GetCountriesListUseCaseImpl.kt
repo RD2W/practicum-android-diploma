62ad8e29c7165c01301f90e9ae83c819764e25d8
Michel-mihim <ya-mihim@yandex.ru>
2025-06-21 10:55:36 +0300

    Сформировал country package

---
+++ app/src/main/java/ru/practicum/android/diploma/country/domain/usecase/impl/GetCountriesListUseCaseImpl.kt
@@ -0,0 +1,14 @@
+package ru.practicum.android.diploma.country.domain.usecase.impl
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.country.domain.model.GetCountriesListResult
+import ru.practicum.android.diploma.country.domain.repository.GetCountriesListRepository
+import ru.practicum.android.diploma.country.domain.usecase.GetCountriesListUseCase
+
+class GetCountriesListUseCaseImpl(
+    private val getCountriesListRepository: GetCountriesListRepository
+) : GetCountriesListUseCase {
+    override fun execute(): Flow<GetCountriesListResult> {
+        return getCountriesListRepository.getCountriesList()
+    }
+}
